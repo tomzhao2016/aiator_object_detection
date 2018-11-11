282ad9ec7b81e28d37d8f5be599603bf4bd53c80
QingyangZhao <276676925@qq.com>
2018-11-11 10:48:58 +0800

    start yolo trail

---
+++ yolo/create_annotations.py
@@ -0,0 +1,5 @@
+ # This file will write the true annotations to a txt file named 'image_annotation'
+ # In the txt, the format should be
+ # path/to/img1.jpg 50,100,150,200,0 30,50,200,120,3
+ # path/to/img2.jpg 120,300,250,600,2
+
